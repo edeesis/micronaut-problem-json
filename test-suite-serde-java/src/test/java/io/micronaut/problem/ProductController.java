e3d98f73ffb828ff6a56e9708c09bfb4c4adebe9
Ed Mitchell <edeesis@gmail.com>
2022-07-11 08:36:00 -0500

    custom exception tests

--- test-suite-serde-java/src/test/java/io/micronaut/problem/ProductController.java
+++ test-suite-serde-java/src/test/java/io/micronaut/problem/ProductController.java
@@ -4,9 +4,13 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Status;
+import io.micronaut.problem.violations.ConstraintViolationThrowableProblem;
+import io.micronaut.problem.violations.Violation;
 import org.zalando.problem.Problem;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller("/product")
 public class ProductController {
@@ -20,5 +24,13 @@ public class ProductController {
                 .withDetail("Item B00027Y5QG is no longer available")
                 .with("product", "B00027Y5QG")
                 .build();
+    }
+
+    @Get("constraint")
+    @Status(HttpStatus.OK)
+    public void constraintViolation() {
+        List<Violation> violations = new ArrayList<>();
+        violations.add(new Violation("foo", "bar"));
+        throw new ConstraintViolationThrowableProblem(new HttpStatusType(HttpStatus.BAD_REQUEST), violations);
     }
 }
